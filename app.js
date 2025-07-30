7596f155100bb24f248490d58c50ef0d4929869e
lazadam <lazadam@hotmail.fr>
2025-07-30 13:32:01 +0200

    Update app.js

--- app.js
+++ app.js
@@ -590,17 +590,3 @@ function formatDate(date) {
         day: 'numeric'
     });
 }
-// src/App.js
-import React from 'react';
-import CalculateurIVT from './CalculateurIVT';
-
-function App() {
-  return (
-    <div className="App">
-      <h1>EasIVT</h1>
-      <CalculateurIVT />
-    </div>
-  );
-}
-
-export default App;
